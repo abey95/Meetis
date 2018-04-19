7724a9a69c6e43d415295b58880c6de6adbcef64
CS3714 <cs3714@torg1010-11.its.vt.edu>
2018-04-19 09:08:38 -0400

    take note segue working

--- Meetis/View Controllers/EventDataViewController.swift
+++ Meetis/View Controllers/EventDataViewController.swift
@@ -28,23 +28,69 @@ class EventDataViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        switch eventDataPassed!.category {
+        case .school:
+            imageView!.image = UIImage(named: "school.png")
+            break
+        case .family:
+            imageView!.image = UIImage(named: "family.png")
+        case .personal:
+            imageView!.image = UIImage(named: "personal.png")
+        case .travel:
+            imageView!.image = UIImage(named: "travel.png")
+        default:
+            imageView!.image = UIImage(named: "work.png")
+            
+        }
+        
+        categoryLabel.text = eventDataPassed!.category.rawValue
+        timeTextField.text = eventDataPassed?.time
+        
+        if eventDataPassed?.priority == "low" {
+            prioritySegmentedControl.selectedSegmentIndex = 0
+        }
+        else if eventDataPassed?.priority == "medium" {
+            prioritySegmentedControl.selectedSegmentIndex = 1
+        }
+        else {
+            prioritySegmentedControl.selectedSegmentIndex = 2
+        }
+        
+        if (eventDataPassed?.days[0])! {
+            sundayButton.backgroundColor = UIColor.blue
+            sundayButton.titleLabel?.textColor = UIColor.white
+        }
+        if (eventDataPassed?.days[1])! {
+            mondayButton.backgroundColor = UIColor.blue
+            mondayButton.titleLabel?.textColor = UIColor.white
+        }
+        if (eventDataPassed?.days[2])! {
+            tuesdayButton.backgroundColor = UIColor.blue
+            tuesdayButton.titleLabel?.textColor = UIColor.white
+        }
+        if (eventDataPassed?.days[3])! {
+            wednesdayButton.backgroundColor = UIColor.blue
+            wednesdayButton.titleLabel?.textColor = UIColor.white
+        }
+        if (eventDataPassed?.days[4])! {
+            thursdayButton.backgroundColor = UIColor.blue
+            thursdayButton.titleLabel?.textColor = UIColor.white
+        }
+        if (eventDataPassed?.days[5])! {
+            fridayButton.backgroundColor = UIColor.blue
+            fridayButton.titleLabel?.textColor = UIColor.white
+        }
+        if (eventDataPassed?.days[6])! {
+            saturdayButton.backgroundColor = UIColor.blue
+            saturdayButton.titleLabel?.textColor = UIColor.white
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
+    }    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
