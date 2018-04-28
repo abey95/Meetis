88df7462b7e50bfd38aa1b66cd2bd11daf19b414
Kyle Thompson <kyle703@vt.edu>
2018-04-28 11:55:39 -0400

    Got the canvas markup working, now going to try to add buttons for changing the pen type

---
+++ Meetis/Note.swift
@@ -0,0 +1,19 @@
+//
+//  Note.swift
+//  Meetis
+//
+//  Created by Kyle Thompson on 4/28/18.
+//  Copyright © 2018 Abey Yoseph. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVKit
+
+class Note: NSObject {
+    
+    var title: String?
+    var views: [UIImage]?
+    var recording: AVAudioSession?
+    var notes: String?
+}
---
+++ Meetis/View Controllers/CanvasView.swift
@@ -0,0 +1,84 @@
+//
+//  CanvasView.swift
+//  Meetis
+//
+//  Created by Kyle Thompson on 4/28/18.
+//  Copyright © 2018 Abey Yoseph. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    var lineColor: UIColor!
+    var lineWidth: CGFloat!
+    var path:UIBezierPath!
+    var touchPoint: CGPoint!
+    var startingPoint: CGPoint!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.clipsToBounds = true
+        self.isMultipleTouchEnabled = false
+        
+        lineColor = UIColor.black
+        lineWidth = 10
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // set the starting point as an initial reference
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
+        
+    }
+    
+    // called when putting finger on surface and moving finger
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // get the value and update touch point to its location
+        let touch = touches.first
+        touchPoint = touch?.location(in: self)
+        
+        // initialize a BezierPath and add a line from the starting point to the current point
+        path = UIBezierPath()
+        path.move(to: startingPoint)
+        path.addLine(to: touchPoint)
+        
+        //update starting point to touch point when finger moved
+        startingPoint = touchPoint
+        
+        //now draw the shape layer
+        drawShapeLayer()
+    }
+    
+    // create a shape over the path the was created
+    func drawShapeLayer() {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        
+        //make line smoother
+        shapeLayer.lineCap = kCALineCapRound
+        
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        
+        self.layer.addSublayer(shapeLayer)
+        self.setNeedsDisplay()
+    }
+    
+    func clearCanvas() {
+        path.removeAllPoints()
+        self.layer.sublayers = nil
+        self.setNeedsDisplay()
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
