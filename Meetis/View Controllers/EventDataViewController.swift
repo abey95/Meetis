0e0160655c45a3d5526c8b4c147bf84c439fbfa9
CS3714 <cs3714@vtcadstation01.cadlab.vt.edu>
2018-05-01 16:24:59 -0400

    edit and save working on event data vc

--- Meetis/View Controllers/EventDataViewController.swift
+++ Meetis/View Controllers/EventDataViewController.swift
@@ -23,11 +23,37 @@ class EventDataViewController: UIViewController {
     @IBOutlet var saturdayButton: UIButton!
     
     
-    var eventDataPassed: Event?
+    @IBOutlet var editSaveButton: UIBarButtonItem!
     
+    let applicationDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    var eventDataPassed: Event?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sundayButton.isEnabled = false
+        mondayButton.isEnabled = false
+        tuesdayButton.isEnabled = false
+        wednesdayButton.isEnabled = false
+        thursdayButton.isEnabled = false
+        fridayButton.isEnabled = false
+        saturdayButton.isEnabled = false
 
+        let navigationBarWidth = self.navigationController?.navigationBar.frame.width
+        let navigationBarHeight = self.navigationController?.navigationBar.frame.height
+        let labelRect = CGRect(x: 0, y: 0, width: navigationBarWidth!, height: navigationBarHeight!)
+        let titleLabel = UILabel(frame: labelRect)
+        
+        titleLabel.text = eventDataPassed?.title
+        
+        titleLabel.font = titleLabel.font.withSize(17)
+        titleLabel.numberOfLines = 1
+        titleLabel.textAlignment = .center
+        titleLabel.lineBreakMode = .byWordWrapping
+        self.navigationItem.titleView = titleLabel
+        
+        
         switch eventDataPassed!.category {
         case .school:
             imageView!.image = UIImage(named: "school.png")
@@ -46,10 +72,10 @@ class EventDataViewController: UIViewController {
         categoryLabel.text = eventDataPassed!.category.rawValue
         timeTextField.text = eventDataPassed?.time
         
-        if eventDataPassed?.priority == "low" {
+        if eventDataPassed?.priority == "Low" {
             prioritySegmentedControl.selectedSegmentIndex = 0
         }
-        else if eventDataPassed?.priority == "medium" {
+        else if eventDataPassed?.priority == "Medium" {
             prioritySegmentedControl.selectedSegmentIndex = 1
         }
         else {
@@ -57,40 +83,93 @@ class EventDataViewController: UIViewController {
         }
         
         if (eventDataPassed?.days[0])! {
-            sundayButton.backgroundColor = UIColor.blue
-            sundayButton.titleLabel?.textColor = UIColor.white
+            sundayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.normal)
         }
         if (eventDataPassed?.days[1])! {
-            mondayButton.backgroundColor = UIColor.blue
-            mondayButton.titleLabel?.textColor = UIColor.white
+            mondayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.normal)
         }
         if (eventDataPassed?.days[2])! {
-            tuesdayButton.backgroundColor = UIColor.blue
-            tuesdayButton.titleLabel?.textColor = UIColor.white
+            tuesdayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.selected)
         }
         if (eventDataPassed?.days[3])! {
-            wednesdayButton.backgroundColor = UIColor.blue
-            wednesdayButton.titleLabel?.textColor = UIColor.white
+            wednesdayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.selected)
         }
         if (eventDataPassed?.days[4])! {
-            thursdayButton.backgroundColor = UIColor.blue
-            thursdayButton.titleLabel?.textColor = UIColor.white
+            thursdayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.selected)
         }
         if (eventDataPassed?.days[5])! {
-            fridayButton.backgroundColor = UIColor.blue
-            fridayButton.titleLabel?.textColor = UIColor.white
+            fridayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.selected)
         }
         if (eventDataPassed?.days[6])! {
-            saturdayButton.backgroundColor = UIColor.blue
-            saturdayButton.titleLabel?.textColor = UIColor.white
+            saturdayButton.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.selected)
         }
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }    
+    }
+    
+    var isEdit = true;
+    
+    @IBAction func editSaveTapped(_ sender: UIBarButtonItem) {
+        
+        if isEdit {
+            
+            editSaveButton.title = "Save"
 
+            sundayButton.isEnabled = true
+            mondayButton.isEnabled = true
+            tuesdayButton.isEnabled = true
+            wednesdayButton.isEnabled = true
+            thursdayButton.isEnabled = true
+            fridayButton.isEnabled = true
+            saturdayButton.isEnabled = true
+            
+        }
+        else {
+            eventDataPassed?.time = timeTextField.text
+            
+            if prioritySegmentedControl.selectedSegmentIndex == 0 {
+                eventDataPassed?.priority = "Low"
+            }
+            else if prioritySegmentedControl.selectedSegmentIndex == 1 {
+                eventDataPassed?.priority = "Med"
+            }
+            else {
+                eventDataPassed?.priority = "High"
+                
+            }
+            
+            applicationDelegate.dict_Events.setValue(eventDataPassed?.toDict(), forKey: (eventDataPassed?.title)!)
 
+            editSaveButton.title = "Edit"
+            
+            sundayButton.isEnabled = false
+            mondayButton.isEnabled = false
+            tuesdayButton.isEnabled = false
+            wednesdayButton.isEnabled = false
+            thursdayButton.isEnabled = false
+            fridayButton.isEnabled = false
+            saturdayButton.isEnabled = false
+            
+        }
+        isEdit = !isEdit
+    }
+    
+    
+    // The weekButtonTapped method is invoked when the user taps the Add button created in viewDidLoad() above.
+    @IBAction func weekButtonTapped(_ sender: UIButton) {
+        
+        // Perform the segue
+        eventDataPassed?.days[sender.tag] = !(eventDataPassed?.days[sender.tag])!
+        
+        if eventDataPassed?.days[sender.tag] == true {
+            sender.setBackgroundImage(UIImage(named: "blue_circle.png"), for: UIControlState.normal)
+        }
+        else {
+            sender.setBackgroundImage(nil, for: UIControlState.normal)
+        }
+    }
 }
+
